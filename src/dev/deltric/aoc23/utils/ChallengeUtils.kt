c3b249323c7191e114e6c9667a0fab6a888cd709
Deltric <d3ltr1c@gmail.com>
2023-12-09 15:50:35 -0600

    Remove unused utility

--- src/dev/deltric/aoc23/utils/ChallengeUtils.kt
+++
@@ -1,8 +0,0 @@
-package dev.deltric.aoc23.utils
-
-import kotlin.io.path.Path
-import kotlin.io.path.readLines
-
-fun readDay(day: Int): List<String> {
-    return Path("./input/day_$day.txt").readLines()
-}
